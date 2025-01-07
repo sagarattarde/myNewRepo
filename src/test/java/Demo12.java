09fdc445dbd859cfef50311b41788ac1233f7513
sagar <sagar>
2025-01-07 13:14:34 +0530

    interface implements

---
+++ src/test/java/Demo12.java
@@ -0,0 +1,15 @@
+public class Demo12 extends Demo{
+    Demo12(){
+        System.out.println("123");
+
+    }Demo12(int a){
+
+        System.out.println("sagar");
+    }
+    public  static void main(String[]args){
+        Demo12 d1=new Demo12();
+        d1.mai(100);
+    Demo12 d2 =new Demo12(200);
+
+    }
+}
